00b465aad0532092eb36d119b1c11a3d7bd646cf
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-05-18 13:04:59 -0700

    port updated

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const client = require('twilio')('AC5d6dabce4797b65a544edc775b8858bb', 'c0a502a6ef22603ce2c3d5cc18dba45f');
 const dbController = require('./dbController');
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect('mongodb://student:ilovetesting@ds143201.mlab.com:43201/todewr');
 mongoose.connection.once('open', () => {
   console.log('Connected to Database');
@@ -19,6 +21,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/sms", dbController.ParseText)
 app.get('/getsms', dbController.getList)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Listening on port 3000');
 });
