4478596fa105937632dbdaf2177291daeed8948f
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-09-08 15:29:51 -0700

    090817 minor bug fixes

--- client/factories/requestFactory.js
+++ client/factories/requestFactory.js
@@ -8,7 +8,7 @@ angular
     let newObj = {};
     newObj.sendMessage = function(inputObj,cb) {
       // console.log(inputObj)
-    $http.get('https://afternoon-reaches-24362.herokuapp.com/getsms', inputObj)
+    $http.get('https://todewr.herokuapp.com/getsms', inputObj)
      .then(function(response) {
       //  console.log(response);
        cb(response);
