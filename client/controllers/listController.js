80005010147f3484528c5f85a15068eac66e9497
Brian Feldman <brianfeldman@Caitlins-MacBook-Pro.local>
2017-09-08 15:18:05 -0700

    favicon added

--- client/controllers/listController.js
+++ client/controllers/listController.js
@@ -1,8 +1,6 @@
-//WIP for web-based list look-up
-
 angular
   .module('ListController', ['ngRoute'])
   .controller('ListController', ListController);
 
-function ListController($scope) {
+function ListController($scope,UserFactory) {
 }
